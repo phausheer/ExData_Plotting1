5cca576cab353987c1e5fd53cb0a1e5c2768b275
Paul <phausheer@chubb.com>
2016-01-09 12:29:14 -0500

    Update EDAWeek1Plot2.R

--- EDAWeek1Plot2.R
+++ EDAWeek1Plot2.R
@@ -5,6 +5,8 @@
 ### File Name - EDAWeek1plot2.R 
 ### *********************************************************************************
 runWeek1Plot2 <- function() {
+  setwd("C:\\Users\\p622403\\gitEDA\\EDA_PlottingW1")
+  print(getwd())
   strFileNa <- "./data/household_power_consumption.txt"
   print(paste0("Reading File:", strFileNa) )
   dfData <- read.table(strFileNa, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
@@ -15,24 +17,31 @@ runWeek1Plot2 <- function() {
   df2Days <- filter(dfData, Date %in% c("1/2/2007","2/2/2007"))
   print(paste0("number of rows:", nrow(df2Days)))
   
-  print("Selecting ONLY 2 columns of data" )
-  df3 <- sqldf("select Date, Global_active_power from df2Days")
-  print(paste0("number of rowsdf3:", nrow(df3)))
-  print(paste0("number of colssdf3:", ncol(df3)))
-  # vcNames <- colnames(df2Days)
+  print("Selecting ONLY 3 columns of data" )
+  df3 <- sqldf("select Date, Time,  Global_active_power from df2Days")
+  print(paste("     number of rows df3:", nrow(df3)))
+  print(paste("     number of colss df3:", ncol(df3)))
+  print(colnames(df3) )
   # View(vcNames)
-  df2Days$Global_active_power <- as.numeric(df2Days$Global_active_power)
-  df2Days$Date <-  as.Date(df2Days$Date, "%Y-%m-%d")
-  df2Days$Time <-  as.Date(df2Days$Time, "%H-%M-%S")
-  df2Days2 <- mutate(df2Days, dateAndTime = paste(Date, Time, sep = ' '))
-  df2Days2$datetimeX <- strptime(paste(df2Days2$Date, df2Days2$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
   
-  # str(df2Days2)
-  print(df2Days[1000:1013,])
+  print("Fixing the column data types" )
+  df3$Global_active_power <- as.numeric(df3$Global_active_power)
+  df3$datetimeX <- strptime(paste(df3$Date, df3$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+  df3$dateDate <-  as.Date(df3$Date, format = "%d/%m/%Y")
+  # df3$dateTime <-  as.Date(df3$Time, format = "%H:%M:%S")
+  df3$PosixltDate <- strptime(df3$dateDate,  "%Y-%m-%d") 
+  df3$PosixltTime <- strptime(df3$Time, "%H:%M:%S")
+  df3 <- mutate(df3, dateAndTime = paste(Date, Time, sep = ' '))
+  df3$PosixltDateTime <- strptime(df3$dateAndTime, format="%d/%m/%Y %H:%M:%S") 
+  
+  # View(colnames(df3))
+  str(df3)
+  print(df3[2:5,])
   # subSetData <- data[data$ ,]
   
   #str(subSetData)
-  # png("plot2.png", width=480, height=480)
-  # hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-  # dev.off()
+  png("plot2.png", width=480, height=480)
+  plot(df3$PosixltDateTime, df3$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  dev.off()
 }
+
